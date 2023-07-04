c06249e439125277ba922e9b2369a4e19a7adc07
TanLeJS <tanlengoc2000@gmail.com>
2023-07-04 15:50:34 +0700

    video 30

--- src/sever.js
+++ src/sever.js
@@ -10,6 +10,10 @@ const hostname = process.env.HOST_NAME
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+//config static file
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 //khai báo route
 app.get('/', (req, res) => {
     res.send('Hello World! and nodemon')
