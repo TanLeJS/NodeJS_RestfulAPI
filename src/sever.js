3d2596df76cff01c18a1e84f3c6bedeb561f1311
TanLeJS <tanlengoc2000@gmail.com>
2023-07-05 14:53:17 +0700

    video 36

--- src/sever.js
+++ src/sever.js
@@ -5,7 +5,7 @@ const webRoutes = require("./routes/web")
 const app = express() // app express
 const port = process.env.PORT || 8888 // port => hardcode
 const hostname = process.env.HOST_NAME
-const mysql = require('mysql2');
+const connection = require("./config/database")
 
 // config template engine
 configViewEngine(app)
@@ -14,23 +14,15 @@ configViewEngine(app)
 app.use('/', webRoutes)
 
 //test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, //default 3306
-    user: 'root',
-    password: '123456', // default empty
-    database: 'hoidanit'
-});
+
 
 // simple query
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
         console.log(">>> result = ", results); // results contains rows returned by server
-        console.log(">>> fields", fields); // fields contains extra meta data about results, if available
     }
 );
-
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
