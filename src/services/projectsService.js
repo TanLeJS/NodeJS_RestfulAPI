f893d204efab8cea44b142aa7ff7acedc595efc5
TanLeJS <tanlengoc2000@gmail.com>
2024-02-01 14:55:14 -0500

    video 115

--- src/services/projectsService.js
+++ src/services/projectsService.js
@@ -5,6 +5,15 @@ const postCreateProjectService = async (data) => {
     let result = await Project.create(data);
     return result;
   }
+  if (data.type === "ADD-USERS") {
+    console.log(">>> check data: ", data);
+    let myProject = await Project.findById(data.projectId).exec();
+    for (let i = 0; i < data.usersArr.length; i++) {
+      myProject.usersInfor.push(data.usersArr[i]);
+    }
+    let result = await myProject.save();
+    return result;
+  }
 };
 
 module.exports = {
